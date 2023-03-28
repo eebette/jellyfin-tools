50ccdd9d9198fad1b20f68cb21c01d948b9deb83
Eric <eric.bette@pm.me>
2023-03-28 16:34:26 +0900

    Pushing patch #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jellyfin-tools",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(include=["cli", "cli.fonts", "install"]),
     package_data={"": ["Prima Sans Bold.otf"]},
     url="https://github.com/eebette/Jellyfin-Tools",
